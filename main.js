c9af881ea8577f36763a93685c7fe0048c9cf164
andreasvidal <andrea.s.vidal00@gmail.com>
2022-07-20 14:25:21 -0400

    slider no funciona solo botones u.u

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
+//SLIDE BANNER
+const d = document
+const slides = d.querySelectorAll('.slide');
+const nextBtn = d.querySelector('.nextBtn');
+const prevBtn = d.querySelector('.prevBtn');
 
-const slice = () =>{
-    const d = document;
-    
-    const imageSlice = d.querySelectorAll('img-banner');
+slides.forEach(function(slide, index){
+    slide.style.left = `${index * 100}%`
+});
+
+let counter = 0;
+nextBtn.addEventListener('click', function (){
+    counter++;
+    carousel()
+})
+
+prevBtn.addEventListener('click', function (){
+    counter--;
+    carousel()
+})
+
+const carousel = () => {
+    if (counter < slides.length - 1){
+        nextBtn.style.display = 'block';
+    }else {
+        nextBtn.style.display = 'none';
+    }
+    if (counter > 0){
+        prevBtn.style.display = 'block';
+    }else {
+        prevBtn.style.display = 'none';
+    }
+    slides.forEach(function (slide){
+        slide.style.transform = `traslatex(-${counter * 100}%)`;
+    });
 }
+prevBtn.style.display = 'none';
+
+//FORM VALIDATION
+
