54fa239362fada3b74b76c18d8631da65ddc3f7c
andreasvidal <andrea.s.vidal00@gmail.com>
2022-07-21 14:17:43 -0400

    modal no muestra datos de usuario

--- main.js
+++ main.js
@@ -1,11 +1,94 @@
-//SLIDE BANNER
+
+//FORM VALIDATION
 const d = document
-const slides = d.querySelectorAll('.slide');
+//function get all id elements
+let id = (id) => d.getElementById(id);
+//function get all class elements
+let classes = (classes) => d.getElementsByClassName(classes);
+let number = (number) => d.getElementsByClassName(number)
+let text = (text) => d.getElementsByClassName(text)
+let btn = d.getElementById("btn");
+
+// VARIABLES DONDE SE ALMACENAN LOS ID Y CLASES DE LAS FUNCIONES ID Y CLASSES
+let username = id('username'),
+lastname = id('lastname'),
+email = id('email'),
+phone = id('phone'),
+age = id('age'),
+form = id('formId'),
+errorMsg = classes('error');
+
+
+//VALIDACIÓN CAMPOS SOLO TEXTO O NUMEROS
+const onlyNum = /[^0-9]/g;
+const onlyStrings = /[^a-zA-ZÑñáéíóúÁÉÍÓÚ\s]*$/g;
+
+//EVENTO PARA MOSTRAR INFORMACION AL USURAIO DE INPUT VACIOS
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    validation(username, 0, 'debes ingresar tu nombre');
+    validation(lastname, 1, 'debes ingresar tu apellido');
+    validation(email, 2, 'debes ingresar tu correo');
+    validation(phone, 3, 'debes ingresar telefono');
+    validation(age,4, 'debes ingresar tu edad');
+})
+
+//FUNCION DE VALIDACIÓN CAMPOS VACIOS
+const validation = (id, index, message) => {
+    if(id.value.trim() === ''){
+        errorMsg[index].innerHTML = message
+        id.style.border = "2px solid red";
+    }else{
+        id.style.border = "2px solid green";
+        errorMsg[index].innerHTML = '';
+    }
+    
+}
+
+//UF DIARIA API
+fetch('https://mindicador.cl/api').then(function(response) {
+    return response.json();
+}).then(function(dailyIndicators) {
+    document.getElementById("UF").innerHTML = 'VALOR UF $ ' + dailyIndicators.uf.valor;
+}).catch(function(error) {
+    console.log('Requestfailed', error);
+});
+
+//MODAL
+let modal = d.getElementById("myModal");
+
+// Span para cerrar modal
+let span = d.getElementsByClassName("close")[0];
+
+// texto modal
+let textModal = d.getElementsByClassName('modalTxt')
+
+// cuando se da click, se abre modal 
+btn.onclick = () => {
+     modal.style.display = "block";
+     let text = modal.createElement('p')
+     text.innerHTML = `Gracias ${username}`
+     textModal.appendChiled(text)
+     console.log(text)
+}
+// cuando el usuario da click a (x), cierra el modal
+span.onclick = () => {
+  modal.style.display = "none";
+}
+// cunado el usuario da click fuera del modal se cierra
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//SLIDE BANNER
+const slides = d.querySelectorAll('.imgBanner');
 const nextBtn = d.querySelector('.nextBtn');
 const prevBtn = d.querySelector('.prevBtn');
 
-slides.forEach(function(slide, index){
-    slide.style.left = `${index * 100}%`
+slides.forEach((slide, index) => {
+    slide.style.transform = `${index * 100}%`
 });
 
 let counter = 0;
@@ -37,70 +120,3 @@ const carousel = () => {
     });
 }
 prevBtn.style.display = 'none';
-
-//FORM VALIDATION
-
-let id = (id) => d.getElementById(id);
-let classes = (classes) => d.getElementsByClassName(classes);
-let number = (number) => d.getElementsByClassName(number)
-
-let username = id('username'),
-lastname = id('lastname'),
-email = id('email'),
-phone = id('phone'),
-age = id('age'),
-form = id('formId'),
-errorMsg = classes('error');
-
-//VALIDACIÓN CAMPOS SOLO TEXTO O NUMEROS
-const onlyNum = /[^0-9]/g;
-const onlyStrings = /[^a-zA-ZÑñáéíóúÁÉÍÓÚ\s]*$/g;
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    validation(username, 0, 'debes ingresar tu nombre');
-    validation(lastname, 1, 'debes ingresar tu apellido');
-    validation(email, 2, 'debes ingresar tu correo');
-    validation(phone, 3, 'debes ingresar telefono');
-    validation(age,4, 'debes ingresar tu edad');
-})
-
-form.addEventListener('keyup', (e) => {
-    e.preventDefault()
-    validNumber (phone, 3, 'solo numeros')
-    validNumber (age, 4, 'solo numeros')
-})
-
-//VALIDACIÓN CAMPOS VACIOS
-const validation = (id, index, message) => {
-    if(id.value.trim() === ''){
-        errorMsg[index].innerHTML = message
-        id.style.border = "2px solid red";
-    }else{
-        id.style.border = "2px solid green";
-        errorMsg.innerHTML = ''
-    }
-    
-}
-
-
-const validNumber = (number, index, message) => {
-    if( number.value!== onlyNum ) {
-        console.log(id.value !== onlyNum)
-        errorMsg[index].innerHTML = message
-    }else{
-        return true;
-    }
-}
-
-
-const validStrings = (number, index, message) => {
-    if( number.value!== onlyNum ) {
-        console.log(id.value !== onlyStrings)
-        errorMsg[index].innerHTML = message
-    }else{
-        return true;
-    }
-}
-
-
